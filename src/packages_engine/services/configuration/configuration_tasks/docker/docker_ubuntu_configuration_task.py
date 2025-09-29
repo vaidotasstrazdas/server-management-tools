13d2a3c5758ca82a34790ba0334a805cb41b3ef9
Vaidotas Strazdas <strazdas.vaidotas@gmail.com>
2025-09-29 23:11:02 +0300

    Docker configuration task introduced. Python formatting libs added.

--- src/packages_engine/services/configuration/configuration_tasks/docker/docker_ubuntu_configuration_task.py
+++ src/packages_engine/services/configuration/configuration_tasks/docker/docker_ubuntu_configuration_task.py
@@ -19,4 +19,74 @@ class DockerUbuntuConfigurationTask(ConfigurationTask):
         self.controller = controller
 
     def configure(self, data: ConfigurationData) -> OperationResult[bool]:
-        return OperationResult[bool].fail('Not supported')
+        create_result = self._create_directories_if_needed([
+            '/srv/gitea',
+            '/srv/postgres',
+            '/srv/pgadmin',
+            '/srv/gitea/data',
+            '/srv/gitea/config',
+            '/srv/postgres/data',
+            '/srv/pgadmin/data',
+            '/srv/stack',
+        ])
+
+        if not create_result.success:
+            return create_result.as_fail()
+
+        self.notifications.info('Reading Docker Config template data.')
+        read_result = self.reader.read(
+            ConfigurationContent.RAW_STRING,
+            data,
+            f'/usr/local/share/{data.server_data_dir}/data/docker-compose.yml'
+        )
+        
+        if not read_result.success or read_result.data == None:
+            self.notifications.error('\tReading Docker Config template data failed.')
+            return read_result.as_fail()
+        self.notifications.success('\tReading Docker Config template data succeeded.')
+
+        self.notifications.info('Writing Docker configuration.')
+        write_result = self.file_system.write_text('/srv/stack/docker-compose.yml', read_result.data)
+        if not write_result.success:
+            self.notifications.error('\tWriting Docker configuration failed.')
+            return write_result.as_fail()
+        self.notifications.success('\tWriting Docker configuration succeeded.')
+
+        self.notifications.info('Starting Docker containers.')
+        start_result = self.controller.run_raw_commands([
+            'sudo chown -R 999:999 /srv/postgres/data',
+            'sudo chown -R 5050:5050 /srv/pgadmin/data',
+            'cd /srv/stack && sudo docker compose up -d'
+        ])
+        if not start_result.success:
+            self.notifications.error('\tFailed to start Docker containers.')
+            return start_result.as_fail()
+        self.notifications.success('\tStarting Docker containers succeeded.')
+
+        return OperationResult[bool].succeed(True)
+    
+    def _create_directories_if_needed(self, directories: list[str]) -> OperationResult[bool]:
+        self.notifications.info('Creating directories if needed.')
+        for directory in directories:
+            create_result = self._create_directory_if_needed(directory)
+            if not create_result.success:
+                self.notifications.error('\tCreating directories operation failed.')
+                return create_result.as_fail()
+        self.notifications.success('\tCreating directories operation succeeded.')
+        
+        return OperationResult[bool].succeed(True)
+    
+    def _create_directory_if_needed(self, directory: str) -> OperationResult[bool]:
+        self.notifications.info(f'\tCreating directory "{directory}" if needed.')
+        if self.file_system.path_exists(directory):
+            self.notifications.success(f'\t\tDirectory "{directory}" exists. No need to create it again.')
+            return OperationResult[bool].succeed(True)
+        
+        self.notifications.info(f'\t\tDirectory "{directory}" does not exist. Creating it now.')
+        create_result = self.controller.run_raw_commands([f'sudo mkdir -p {directory}'])
+        if not create_result.success:
+            self.notifications.error(f'\t\tCreation of the directory "{directory}" failed.')
+            return create_result.as_fail()
+        self.notifications.success(f'\t\tCreation of the directory "{directory}" succeeded.')
+
+        return OperationResult[bool].succeed(True)
--- src/packages_engine/services/package_controller/package_controller_service_mock.py
+++ src/packages_engine/services/package_controller/package_controller_service_mock.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, Dict
 
 from packages_engine.models import OperationResult
 
@@ -18,6 +18,7 @@ class MockPackageControllerService(PackageControllerServiceContract):
         self.run_raw_command_params: list[str] = []
         self.run_raw_commands_params: list[list[str]] = []
         self.run_raw_commands_result = OperationResult[bool].succeed(True)
+        self.run_raw_commands_result_regex_map: Dict[str, OperationResult[bool]] = {}
 
     def install_package(self, package: str):
         self.install_package_params.append(package)
@@ -33,4 +34,17 @@ class MockPackageControllerService(PackageControllerServiceContract):
 
     def run_raw_commands(self, commands: list[str]) -> OperationResult[bool]:
         self.run_raw_commands_params.append(commands)
+
+        for command in commands:
+            print(command)
+            result_found = self._find_result(command)
+            if result_found != None:
+                return result_found
+
         return self.run_raw_commands_result
+    
+    def _find_result(self, command: str) -> OperationResult[bool] | None:
+        for key in self.run_raw_commands_result_regex_map.keys():
+            if command in key:
+                return self.run_raw_commands_result_regex_map[key]
+        return None
